a004170e782fcc4dad0b109d72ccc7eeb6c2bca7
Jim Hinkle <BreezyGlade@proton.me>
2026-02-04 05:15:21 -0600

    removed duplicate entries from messages.py

--- messages.py
+++ messages.py
@@ -7,43 +7,11 @@ A door leads to the West.
 {separator}
 --------------Press 'h' for Help---Press 'q' to Quit--------------"""
 
-note_message = f"""{separator}
-The note reads:
-You will need to collect these items to escape: Rope, Shield,
-Grappling Hook, Flashstone, and Broom. Without these items and
-this note, you are surely doomed!
-{separator}"""
-
 help_message = f"""{separator}
 Input map:
 Movement: n - North, e - East, s - South, w - West
 Actions:  p - Pick up item, i - Display inventory, q - Quit game
 {separator}"""
-
-
-win_message = """ As you enter the room you see the terrible Red Dragon
-across the chasm breath in and blast his molten breath
-in your direction! You quickly raise your shield and
-narrowly avoid incineration. He breathes in, preparing
-another attack, giving you a split second with which to
-hurl the flashstone before his eyes. It explodes, blinding
-the wyrm. You quickly search for a grapple point so
-you can swing across the wide chasm, but can't find
-anything besides a small recess that once held a torch.
-Thinking quickly you aim the broom for it, and by what
-could only be described as divine providence, your aim
-is true. As quickly as you can, you use it to swing 
-to the other side before as the cursing dragon begins to
-regain his vision. Thankfully, the door to your freedom
-is within reach, and you escape to live another day."""
-separator = "-" * 66
-
-opening_description = f"""Welcome to Dungeon Escape Quest: Ultra!!
-{separator}
-You wake up in a cold, damp room. A single candle candle burns.
-A door leads to the West.
-{separator}
---------------Press 'h' for Help---Press 'q' to Quit--------------"""
 
 note_message = f"""{separator}
 The note reads:
@@ -51,13 +19,6 @@ You will need to collect these items to escape: Rope, Shield,
 Grappling Hook, Flashstone, and Broom. Without these items and
 this note, you are surely doomed!
 {separator}"""
-
-help_message = f"""{separator}
-Input map:
-Movement: n - North, e - East, s - South, w - West
-Actions:  p - Pick up item, i - Display inventory, q - Quit game
-{separator}"""
-
 
 win_message = """ As you enter the room you see the terrible Red Dragon
 across the chasm breath in and blast his molten breath
